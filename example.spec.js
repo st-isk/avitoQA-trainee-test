f25cb87030555ef38b59d4d10e1fb057522edf49
Alexander <69770361+st-isk@users.noreply.github.com>
2024-04-14 04:01:10 +0300

    Playwright autotests

---
+++ example.spec.js
@@ -0,0 +1,84 @@
+// @ts-check
+const { test } = require("@playwright/test");
+const { devices } = require("playwright");
+
+test("check CO2 counter on desktop viewport", async ({ browser }) => {
+    const page = await browser.newPage();
+    await page.setViewportSize({
+        width: 1920,
+        height: 1080,
+    });
+    await page.goto("https://www.avito.ru/avito-care/eco-impact");
+
+    await page
+        .locator(".desktop-impact-item-eeQO3")
+        .filter({ has: page.getByText("не попало в атмосферу") })
+        .screenshot({ path: "output/ТК1.png" });
+});
+
+test("check water counter on desktop viewport", async ({ browser }) => {
+    const page = await browser.newPage();
+    await page.setViewportSize({
+        width: 1920,
+        height: 1080,
+    });
+    await page.goto("https://www.avito.ru/avito-care/eco-impact");
+
+    await page
+        .locator(".desktop-impact-item-eeQO3")
+        .filter({ has: page.getByText("было сохранено") })
+        .screenshot({ path: "output/ТК2.png" });
+});
+
+test("check energy counter on desktop viewport", async ({ browser }) => {
+    const page = await browser.newPage();
+    await page.setViewportSize({
+        width: 1920,
+        height: 1080,
+    });
+    await page.goto("https://www.avito.ru/avito-care/eco-impact");
+
+    await page
+        .locator(".desktop-impact-item-eeQO3")
+        .filter({ has: page.getByText("было сэкономлено") })
+        .screenshot({ path: "output/ТК3.png" });
+});
+
+test("check CO2 counter on monile viewport", async ({ browser }) => {
+    const context = await browser.newContext({
+        ...devices["iPhone 11 Pro"],
+    });
+    const page = await context.newPage();
+    await page.goto("https://www.avito.ru/avito-care/eco-impact");
+
+    await page
+        .locator(".mobile-impact-item-k2hNC")
+        .filter({ has: page.getByText("не попало в атмосферу") })
+        .screenshot({ path: "output/ТК4.png" });
+});
+
+test("check water counter on monile viewport", async ({ browser }) => {
+    const context = await browser.newContext({
+        ...devices["iPhone 11 Pro"],
+    });
+    const page = await context.newPage();
+    await page.goto("https://www.avito.ru/avito-care/eco-impact");
+
+    await page
+        .locator(".mobile-impact-item-k2hNC")
+        .filter({ has: page.getByText("было сохранено") })
+        .screenshot({ path: "output/ТК5.png" });
+});
+
+test("check energy counter on monile viewport", async ({ browser }) => {
+    const context = await browser.newContext({
+        ...devices["iPhone 11 Pro"],
+    });
+    const page = await context.newPage();
+    await page.goto("https://www.avito.ru/avito-care/eco-impact");
+
+    await page
+        .locator(".mobile-impact-item-k2hNC")
+        .filter({ has: page.getByText("было сэкономлено") })
+        .screenshot({ path: "output/ТК6.png" });
+});
